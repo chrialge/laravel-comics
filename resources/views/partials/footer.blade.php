f606c10649f101962caf5540f254b668c30ab21e
chrialge <chrialge@gmail.com>
2024-05-07 19:16:24 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,34 +1,97 @@
-<footer class="bg-dark text-light text-center text-lg-start">
-    <div class="container p-4">
-        <div class="row">
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer text</h5>
+<footer class="text-white ">
+    <div class="footer_top py-5">
+        <div class="container">
 
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                    voluptatem veniam, est atque cumque eum delectus sint!
-                </p>
+            <div class="row row-cols-1 row-cols-md-2">
+                <div class="col">
+                    <div class="row row-cols-3">
+
+                        <div class="col">
+
+                            <ul class=" list-unstyled">
+                                <li>
+                                    <h3>DC COMICS</h3>
+                                </li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Character</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Comics</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Movies</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">TV</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Games</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Videos</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">News</a></li>
+                            </ul>
+
+                            <ul class=" list-unstyled">
+                                <li>
+                                    <h3>SHOP</h3>
+                                </li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Shop DC</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Shop DC Collectibles</a></li>
+
+                            </ul>
+                        </div>
+
+                        <div class="col">
+
+                            <ul class=" list-unstyled">
+                                <li>
+                                    <h3>DC COMICS</h3>
+                                </li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Term Of Use</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Privacy policy(New)</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Ad Choices</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Advertising</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Jobs</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Subscriptions</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Talent Workshop</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">CPSC Certificates</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Ratings</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Shop Help</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">Contact Us</a></li>
+                            </ul>
+                        </div>
+
+                        <div class="col">
+                            <ul class=" list-unstyled">
+                                <li>
+                                    <h3>SITES</h3>
+                                </li>
+                                <li><a href="#" class=" text-decoration-none text_footer">DC</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">MAD Magazine</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">DC Kids</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">DC Universe</a></li>
+                                <li><a href="#" class=" text-decoration-none text_footer">DC Power Visa</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <p class="text_footer">
+                        All Site Content TM and &copy; 2020 DC Entertainment, unless otherwisw <a href="#" class=" text-decoration-none">noted here</a>. All rights reserved.
+                        <a href="#" class=" text-decoration-none">Cokies Settings</a>
+                    </p>
+                </div>
+                <div class="col logo_footer">
+                    <img src="{{Vite::asset('resources\img\dc-logo-bg.png')}}" style="width: 550px;" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer_bottom bg-dark py-5">
+        <div class="container d-flex justify-content-between">
+            <div class="sign_up">
+                <button class="btn text-white rounded-0">SIGN-UP NOW</button>
+            </div>
+            <div class="follow d-flex align-items-center">
+                <h5 style="width: 120px;">FOLLOW US</h5>
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png')}}" class="px-1" alt="">
+                <img src="{{Vite::asset('resources\img\footer-twitter.png')}}" class="px-1" alt="">
+                <img src="{{Vite::asset('resources\img\footer-youtube.png')}}" class="px-1" alt="">
+                <img src="{{Vite::asset('resources\img\footer-pinterest.png')}}" class="px-1" alt="">
+                <img src="{{Vite::asset('resources\img\footer-periscope.png')}}" class="px-1" alt="">
+
             </div>
 
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer text</h5>
-
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                    voluptatem veniam, est atque cumque eum delectus sint!
-                </p>
-            </div>
         </div>
 
     </div>
-
-
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
-        © 2020 Copyright:
-        <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-    </div>
-    <!-- Copyright -->
 </footer>
